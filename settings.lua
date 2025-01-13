606c76de44a83551a292be0ecec4dae9cfb05f98
LuminaSapphira <1103450+LuminaSapphira@users.noreply.github.com>
2025-01-13 12:41:10 -0500

    Add Early data stage loading

--- settings.lua
+++ settings.lua
@@ -4,6 +4,15 @@ data:extend {
         name = "settings-embedded-data-code-data-stage-code",
         setting_type = "startup",
         default_value = "",
-        allow_blank = true
-    }
+        allow_blank = true,
+        order = "b",
+    },
+    {
+        type = "string-setting",
+        name = "settings-embedded-data-code-early-data-stage-code",
+        setting_type = "startup",
+        default_value = "",
+        allow_blank = true,
+        order = "a",
+    },
 }
